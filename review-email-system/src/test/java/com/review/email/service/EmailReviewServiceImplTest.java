938234f3a8362f0037e11c6e9dd0080a1d1e91bd
alessandrocolleoni <colleoni.al@gmail.com>
2017-08-06 01:41:33 +0200

    Bugfix
    Fixed the usage of mockito into a test because it was passing through
    eclipse but not with maven package.

--- review-email-system/src/test/java/com/review/email/service/EmailReviewServiceImplTest.java
+++ review-email-system/src/test/java/com/review/email/service/EmailReviewServiceImplTest.java
@@ -1,8 +1,11 @@
 package com.review.email.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Rule;
@@ -16,7 +19,9 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.review.email.domain.EmailReview;
@@ -74,8 +79,57 @@ public class EmailReviewServiceImplTest {
 		this.emailReviewService.save(new EmailReview("456", new Date(1234), "email@email.com", "email", "subject", "body", 0.0, false));
 		this.emailReviewService.save(new EmailReview("789", new Date(5678), "email@email.com", "email", "subject", "body", 0.0, false));
 		
-		Page<EmailReview> emailReviews = this.emailReviewService.findAllByPageOrderByDate(Matchers.<Pageable>anyObject());	
-		assertThat(emailReviews.getContent().size()).isEqualTo(3);
+		Page<EmailReview> emailReviewsDb = this.emailReviewService.findAllByPageOrderByDate(new Pageable() {
+
+			@Override
+			public int getPageNumber() {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public int getPageSize() {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public int getOffset() {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public Sort getSort() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public Pageable next() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public Pageable previousOrFirst() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public Pageable first() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public boolean hasPrevious() {
+				// TODO Auto-generated method stub
+				return false;
+			}});
+		
+		assertThat(emailReviewsDb.getContent().size()).isEqualTo(3);
 	}
 	
 	@Test
@@ -84,7 +138,55 @@ public class EmailReviewServiceImplTest {
 		this.emailReviewService.save(new EmailReview("456", new Date(1234), "email@email.com", "email", "subject", "body", 0.0, true));
 		this.emailReviewService.save(new EmailReview("789", new Date(5678), "email@email.com", "email", "subject", "body", 0.0, false));
 		
-		Page<EmailReview> emailReviews = this.emailReviewService.findByPublished(Matchers.<Pageable>anyObject(), true);	
+		Page<EmailReview> emailReviews = this.emailReviewService.findByPublished(new Pageable() {
+
+			@Override
+			public int getPageNumber() {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public int getPageSize() {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public int getOffset() {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public Sort getSort() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public Pageable next() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public Pageable previousOrFirst() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public Pageable first() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public boolean hasPrevious() {
+				// TODO Auto-generated method stub
+				return false;
+			}}, true);	
 		assertThat(emailReviews.getContent().size()).isEqualTo(1);
 	}
 
