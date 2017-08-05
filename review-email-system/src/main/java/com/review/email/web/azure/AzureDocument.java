d2c44482752b8895c937b68669fb5828330c0e22
alessandrocolleoni <colleoni.al@gmail.com>
2017-08-05 18:55:18 +0200

    Azure classes
    Defined the classes used by azure

--- review-email-system/src/main/java/com/review/email/web/azure/AzureDocument.java
+++ review-email-system/src/main/java/com/review/email/web/azure/AzureDocument.java
@@ -1,5 +1,40 @@
 package com.review.email.web.azure;
 
 public class AzureDocument {
+	
+	private String language;
+	private String id;
+	private String text;
+	
+	public AzureDocument(String language, String id, String text) {
+		super();
+		this.language = language;
+		this.id = id;
+		this.text = text;
+	}
 
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
 }
--- review-email-system/src/main/java/com/review/email/web/azure/AzureDocuments.java
+++ review-email-system/src/main/java/com/review/email/web/azure/AzureDocuments.java
@@ -1,5 +1,22 @@
 package com.review.email.web.azure;
 
-public class AzureDocuments {
+import java.util.List;
 
+public class AzureDocuments {
+	private List<AzureDocument> documents;
+
+	public AzureDocuments(List<AzureDocument> documents) {
+		super();
+		this.documents = documents;
+	}
+
+	public List<AzureDocument> getDocuments() {
+		return documents;
+	}
+
+	public void setDocuments(List<AzureDocument> documents) {
+		this.documents = documents;
+	}
+	
+	
 }
